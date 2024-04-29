42faf5ff8f6d2ae09c4838efc4841b06388accaf
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2024-04-29 14:21:54 -0400

    config

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'Docs',
+  theme: {
+    colorScheme: 'system',
+    variables: {
+      color: {
+        textAccent: { light: '#2394ff', dark: '#e9e9ea' },
+        backgroundDark: {light: '#ebeaee', dark: 'black'},
+        background: {light: '#f6f5f8', dark: 'black'}, //f6f5f8 /ebf1f8
+        heading: { light: 'black', dark: '#e9e9ea' },
+        text3: { light: 'black', dark: '#e9e9ea' },
+        background5: { light: '#dee8ff', dark: '#3c393f' },
+      }
+    }
+  },
+  title: 'EIP-5792',
   sidebar: [
     {
       text: 'Getting Started',
