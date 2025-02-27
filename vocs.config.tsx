9fd9cce882d59322202bfd35edd92c891a5106b9
azf20 <azfuller20@gmail.com>
2025-02-27 14:25:01 +0000

    fix up for latest changes

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -1,117 +1,118 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'EIP-5792',
-  titleTemplate: '%s · EIP-5792',
-  description: 'Next-gen wallet RPCs',
-  ogImageUrl: 'https://vocs.dev/api/og?title=%title&description=%description',
+  title: "EIP-5792",
+  titleTemplate: "%s · EIP-5792",
+  description: "Next-gen wallet RPCs",
+  ogImageUrl: "https://vocs.dev/api/og?title=%title&description=%description",
   head() {
     return (
       <>
         <script
-          dangerouslySetInnerHTML={{ __html: 'window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };'}}
+          dangerouslySetInnerHTML={{
+            __html:
+              "window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };",
+          }}
         />
         <script defer src="/_vercel/insights/script.js" />
       </>
-    )
+    );
   },
   theme: {
     accentColor: {
-      light: '#73F7FF',
-      dark: '#73F7FF',
+      light: "#73F7FF",
+      dark: "#73F7FF",
     },
     variables: {
       color: {
         background: {
-          light: 'white',
-          dark: '#151A26',
+          light: "white",
+          dark: "#151A26",
         },
         backgroundDark: {
-          light: '#F5F5F5',
-          dark: '#0F131E',
+          light: "#F5F5F5",
+          dark: "#0F131E",
         },
         textAccent: {
-          light: '#050F19',
-          dark: 'white',
+          light: "#050F19",
+          dark: "white",
         },
       },
     },
   },
-  topNav: [ 
-    { text: 'EIP', link: 'https://eips.ethereum.org/EIPS/eip-5792' }, 
-  ], 
+  topNav: [{ text: "EIP", link: "https://eips.ethereum.org/EIPS/eip-5792" }],
   socials: [
-    { 
-      icon: 'github', 
-      link: 'https://github.com/lukasrosario/eip-5792-site', 
-    }, 
+    {
+      icon: "github",
+      link: "https://github.com/lukasrosario/eip-5792-site",
+    },
   ],
   sidebar: [
     {
-      text: 'Introduction',
-      link: '/introduction',
+      text: "Introduction",
+      link: "/introduction",
     },
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      text: "Getting Started",
+      link: "/getting-started",
     },
-    { 
-      text: 'Reference', 
-      collapsed: false, 
-      items: [ 
-        { 
-          text: 'wallet_getCapabilities', 
-          link: '/reference/getCapabilities', 
-        }, 
-        { 
-          text: 'wallet_sendCalls', 
-          link: '/reference/sendCalls', 
-        }, 
-        { 
-          text: 'wallet_getCallsStatus', 
-          link: '/reference/getCallsStatus', 
-        }, 
-        { 
-          text: 'wallet_showCallsStatus', 
-          link: '/reference/showCallsStatus', 
-        },
-      ], 
+    {
+      text: "Future Development",
+      link: "/future-development",
     },
-    { 
-      text: 'Capabilities', 
-      collapsed: false, 
-      items: [ 
-        { 
-          text: 'atomicBatch', 
-          link: '/capabilities/atomicBatch', 
-        }, 
-        { 
-          text: 'paymasterService', 
-          link: '/capabilities/paymasterService', 
-        }, 
-        { 
-          text: 'auxiliaryFunds', 
-          link: '/capabilities/auxiliaryFunds', 
-        }, 
-      ], 
-    },
-    { 
-      text: 'Ecosystem', 
-      collapsed: false, 
-      items: [ 
-        { 
-          text: 'Wallets', 
-          link: '/ecosystem/wallets', 
+    {
+      text: "Reference",
+      collapsed: false,
+      items: [
+        {
+          text: "wallet_getCapabilities",
+          link: "/reference/getCapabilities",
         },
         {
-          text: 'Tools',
-          link: '/ecosystem/tools',
+          text: "wallet_sendCalls",
+          link: "/reference/sendCalls",
         },
-        { 
-          text: 'Apps', 
-          link: '/ecosystem/apps', 
+        {
+          text: "wallet_getCallsStatus",
+          link: "/reference/getCallsStatus",
         },
-      ], 
+        {
+          text: "wallet_showCallsStatus",
+          link: "/reference/showCallsStatus",
+        },
+      ],
+    },
+    {
+      text: "Ecosystem",
+      collapsed: false,
+      items: [
+        {
+          text: "Wallets",
+          link: "/ecosystem/wallets",
+        },
+        {
+          text: "Tools",
+          link: "/ecosystem/tools",
+        },
+        {
+          text: "Apps",
+          link: "/ecosystem/apps",
+        },
+      ],
+    },
+    {
+      text: "Capabilities",
+      collapsed: false,
+      items: [
+        {
+          text: "paymasterService",
+          link: "/capabilities/paymasterService",
+        },
+        {
+          text: "flowControl",
+          link: "/capabilities/flowControl",
+        },
+      ],
     },
   ],
-})
+});
